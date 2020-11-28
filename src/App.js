3c21cecd509047e71552301cc2f76b1e71c719fa
Brainmaniac <olof.jondelius@gmail.com>
2020-11-28 16:06:44 +0100

    Test if it works again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="flex w-full justify-center">
         <p className="mt-8 font-mono text-5xl font-black text-red-500">
-          Zombie Turret!
+          Zombie Turret!!
         </p>
     </div>
   );
