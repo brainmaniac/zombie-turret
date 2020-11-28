79bd81b79616d05f4f8907158f4c254455f4c31a
Anders Jurisoo <jurisoo@hotmail.com>
2020-11-28 14:49:50 +0100

    Added basic tailwind :rocket:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './tailwind.output.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://user-images.githubusercontent.com/13656814/100516627-75b2de00-3185-11eb-8beb-65ffc08bcf24.png" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className="flex w-full justify-center">
+        <p className="mt-8 font-mono text-5xl font-black text-red-500">
           Zombie Turret
-        </a>
-      </header>
+        </p>
     </div>
   );
 }
