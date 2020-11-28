86c995593bf6a4f8b957032fa1262b3a4c548998
Brainmaniac <olof.jondelius@gmail.com>
2020-11-28 20:41:31 +0100

    Add button in bottom

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './tailwind.output.css';
 
 import Header from './Header';
 import Map from './Map';
+import BuildButton from './BuildButton';
 
 function App() {
   return (
     <div className="flex flex-col w-full justify-center">
         <Header />
         <Map />
+        <BuildButton />
     </div>
   );
 }
