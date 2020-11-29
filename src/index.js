5c0186789f4894a2bac09f43f69d7313c66c03e8
Olof Jondelius <brainmaniac@users.noreply.github.com>
2020-11-29 14:59:14 +0100

    Back to basic (#2)
    * wip
    
    * Back to where we were

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import mapboxgl from 'mapbox-gl';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -21,3 +12,34 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//Set the correct size of the map
+let mapContainer = document.getElementById("map");
+let availableHeight =  (window.innerHeight - 64) + 'px';
+let availableWidth = window.innerWidth + 'px';
+
+mapContainer.style.width = availableWidth;
+mapContainer.style.height = availableHeight;
+
+// Import the map
+mapboxgl.accessToken = 'pk.eyJ1IjoianVyaXNvbyIsImEiOiIzNmVlOTA1YTVlNjliNmE5ZmYyZTA0M2NhZjlkZmFhYyJ9.B_qrLs29LUIpcJaOLSEk2A';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
+    center: [18, 59], // starting position [lng, lat]
+    zoom: 20 // starting zoom
+});
+
+let geoLocate = new mapboxgl.GeolocateControl({
+  positionOptions: {
+  enableHighAccuracy: true
+  },
+  trackUserLocation: true
+})
+
+map.addControl(geoLocate);
+
+map.on('load', () => {
+  geoLocate.trigger()
+} )
